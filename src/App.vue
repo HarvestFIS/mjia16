6bfaf5a1aa640c4ae346e59bbca34786851ad931
王新雷 <173311763@qq.com>
2018-02-08 16:39:14 +0800

    添加动画，并增加删掉的文件

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+	<div id="app">
+		<!-- <transition :name="'vux-pop-' + (direction === 'forward' ? 'in' : 'out')">
+      <keep-alive>
+        <router-view v-if="!$route.meta.keepAlive"></router-view>
+      </keep-alive>
+    </transition> -->
+    <transition :name="'vux-pop-' + (direction === 'forward' ? 'in' : 'out')">
+        <router-view></router-view>
+    </transition>
+	</div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex'
+export default {
+  computed: {
+    ...mapState({
+      direction: state => state.vux.direction
+    })
+  }
+}
+</script>
+
+<style lang = 'less'>
+@import '~vux/src/styles/reset.less';
+@import '~vux/src/styles/1px.less';
+@import url(./common/less/common.less);
+@import url(./common/less/base.less);
+.vux-pop-out-enter-active,
+.vux-pop-out-leave-active,
+.vux-pop-in-enter-active,
+.vux-pop-in-leave-active {
+  will-change: transform;
+  transition: all 500ms;
+  height: 100%;
+  width: 100%;
+  position: absolute; 
+  backface-visibility: hidden;
+  perspective: 1000;
+}
+.vux-pop-out-enter {
+  opacity: 0;
+  transform: translate3d(-100%, 0, 0);
+}
+.vux-pop-out-leave-active {
+  opacity: 0;
+  transform: translate3d(100%, 0, 0);
+}
+.vux-pop-in-enter {
+  opacity: 0;
+  transform: translate3d(100%, 0, 0);
+}
+.vux-pop-in-leave-active {
+  opacity: 0;
+  transform: translate3d(-100%, 0, 0);
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,21 @@ const page1 = r => require.ensure([], () => r(require('../page/page1.vue')),'pag
 const page2 = r => require.ensure([], () => r(require('../page/page2.vue')),'page2')
 const page3 = r => require.ensure([], () => r(require('../page/page3.vue')),'page3')
 
+
+// 引入状态管理
+store.registerModule('vux', {
+  state: {
+    direction: 'forward'
+  },
+  mutations: {
+    updateDirection(state, payload) {
+      state.direction = payload.direction
+    }
+  }
+})
+Vue.use(store)  
+
+
 const router = new Router({
   routes: [
     {
@@ -34,11 +49,59 @@ const router = new Router({
   ]
 })
 
-//这里可以增加登录校验
+
+// history management
+const history = window.sessionStorage
+history.clear()
+let historyCount = history.getItem('count') * 1 || 0
+history.setItem('/', 0)
 router.beforeEach((to, from, next) => {
-	console.log("router beforeEach");
-	console.log("get->"+store.state.count);
-	next();
+  // store.commit('updateLoadingStatus', {
+  //   isLoading: true
+  // })
+  const toIndex = history.getItem(to.path)
+  const fromIndex = history.getItem(from.path)
+  // console.log("toIndex >", toIndex)
+  // console.log("fromIndex >", fromIndex)
+  // console.log(historyCount)
+  if (toIndex) {
+    if (!fromIndex || parseInt(toIndex, 10) > parseInt(fromIndex, 10) || (toIndex === '0' && fromIndex === '0')) {
+      store.commit('updateDirection', {
+        direction: 'forward'
+      })
+    } else {
+      store.commit('updateDirection', {
+        direction: 'reverse'
+      })
+    }
+  } else {
+    ++historyCount
+    history.setItem('count', historyCount)
+    to.path !== '/' && history.setItem(to.path, historyCount)
+    store.commit('updateDirection', {
+      direction: 'forward'
+    })
+  }
+  // console.log('路由开始')
+  /**
+   * 这里设置需要拦截的页面
+   */
+  if (to.matched.some(res => res.meta.requireAuth)) {
+    let token = getStore('userToken')
+    if (token && token !== 'null') {
+      next()
+    } else {
+      next({
+        path: '/login'
+      })
+    }
+  } else {
+    next()
+  }
+})
+
+router.afterEach(function (to) {
+  // console.log('路由结束')
 })
 
 
